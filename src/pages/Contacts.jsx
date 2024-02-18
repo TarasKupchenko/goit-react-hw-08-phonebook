34b0395c508e1df3ed259291ff7848a8742b6164
TarasKupchenko <133959505+TarasKupchenko@users.noreply.github.com>
2024-02-18 17:20:15 +0200

    start

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -2,14 +2,12 @@ import { ContactList } from 'components/ContactList/ContactList';
 import { Filter } from 'components/Filter/Filter';
 import { FormAddContacts } from 'components/FormAddContacts/FormAddContacts';
 import React from 'react';
-import { Helmet } from 'react-helmet';
 
 const Contacts = () => {
   return (
     <div>
-      <Helmet>
-        <title>Contacts</title>
-      </Helmet>
+      <title>Contacts</title>
+
       <h1 className="page-title">Phonebook</h1>
       <FormAddContacts />
       <h2 className="contact-title">Contacts</h2>
